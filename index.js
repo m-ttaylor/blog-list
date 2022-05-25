cff637c1d201e78b2d4fa8da89d5c214cba1855f
m-ttaylor <mttaylor@email.wm.edu>
2022-05-25 11:13:33 -0400

    Refactor blog model

--- index.js
+++ index.js
@@ -4,15 +4,9 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const Blog = require('./models/blog')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+// const Blog = mongoose.model('Blog', blogSchema)
 
 // const mongoUrl = 'mongodb://localhost/bloglist'
 const mongoUrl = config.MONGODB_URI
