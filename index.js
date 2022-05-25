c148bd560549099421f54c5d06d8e8cb1429cc8e
m-ttaylor <mttaylor@email.wm.edu>
2022-05-25 11:28:10 -0400

    separate app, index, and blogs controller

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+// const logger = require('./utils/logger')
+
+const server = http.createServer(app)
 
 // const Blog = mongoose.model('Blog', blogSchema)
 
 // const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl = config.MONGODB_URI
 
-mongoose.connect(mongoUrl)
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
