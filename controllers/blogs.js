a043c519984d2a3c87ea670eaf5898a5ebec995b
m-ttaylor <mttaylor@email.wm.edu>
2022-06-01 13:55:11 -0400

    Add token authorization and require for POSTing blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,8 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const BlogUser = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate(
     'user', { username: 1, name: 1 }
@@ -9,12 +11,26 @@ blogsRouter.get('/', async (request, response) => {
   return response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const users = await BlogUser.find({})
-  const user = users[0]
-
-  console.log('users found are', users)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      'error': 'token missing or invalid'
+    })
+  }
+  const user = await BlogUser.findById(decodedToken.id)
+  console.log('user found is', user)
+  
   const blog = new Blog({
     title: body.title,
     author: body.author,
